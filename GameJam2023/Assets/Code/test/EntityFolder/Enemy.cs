64b4ab9fafb09abab642a2b3de05797c9b9109d9
Jacob <kubagaczko102@wp.pl>
2023-07-04 21:52:25 +0200

    update Enemy.cs

--- GameJam2023/Assets/Code/test/EntityFolder/Enemy.cs
+++ GameJam2023/Assets/Code/test/EntityFolder/Enemy.cs
@@ -4,9 +4,28 @@ using UnityEngine;
 
 public class Enemy : Entity
 {
-    public void Start() 
+    public GameObject TargetObject; 
+    public Rigidbody2D thisRigidBody;
+   
+    void Start()
     {
-        Debug.Log("dupa");           
+        Debug.Log("Dupa");
+        thisRigidBody = this.GetComponent<Rigidbody2D>();
     }
-    
+
+    void FixedUpdate()
+    {
+        if (TargetObject != null)
+        {
+            Follow(TargetObject);
+        }
+    }
+
+    void Follow(GameObject myObject)
+    {
+        Vector2 Direction = myObject.transform.position - transform.position;
+        Vector2 NormalizedDirection = Direction.normalized;
+       //transform.position = NewPosition;
+        thisRigidBody.MovePosition(thisRigidBody.position + NormalizedDirection * Speed * Time.fixedDeltaTime);// * Speed * Time.deltaTime);
+    }
 }
