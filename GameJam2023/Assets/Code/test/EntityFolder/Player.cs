bae16a55758560202be1c96538c631de814c7448
kffans <kubakwan@gmail.com>
2023-07-04 19:47:58 +0200

    Working rigidbody

--- GameJam2023/Assets/Code/test/EntityFolder/Player.cs
+++ GameJam2023/Assets/Code/test/EntityFolder/Player.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class Player : Entity
 {
-    void Start()
+    Rigidbody2D thisRigidBody;
+	void Start()
     {
-        Speed = 400f;        
+        thisRigidBody = this.GetComponent<Rigidbody2D>();
+		Speed = 400f;        
     }
     
 
-    void Update()
+    void FixedUpdate()
     {
 
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector2 movement = new Vector3(moveHorizontal, moveVertical);
-        transform.Translate(movement * Speed * Time.deltaTime);
+        //transform.Translate(movement * Speed * Time.deltaTime);
+        thisRigidBody.MovePosition(thisRigidBody.position + movement * Speed * Time.fixedDeltaTime);// * Speed * Time.deltaTime);
     }
 }
