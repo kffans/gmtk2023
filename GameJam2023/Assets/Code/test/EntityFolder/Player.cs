790df58116200df95392fa63aca8f4c9240de4ff
Jacob <kubagaczko102@wp.pl>
2023-07-05 21:05:19 +0200

    Update Player.cs

--- GameJam2023/Assets/Code/test/EntityFolder/Player.cs
+++ GameJam2023/Assets/Code/test/EntityFolder/Player.cs
@@ -1,25 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : Entity
 {
-    Rigidbody2D thisRigidBody;
-	void Start()
+    public TextMeshProUGUI HealthText;
+    public TextMeshProUGUI ScoreText;
+    Rigidbody2D ThisRigidbody;
+    public int Score = 0;
+
+    void Start()
     {
-        thisRigidBody = this.GetComponent<Rigidbody2D>();
-		Speed = 400f;        
+        ThisRigidbody = GetComponent<Rigidbody2D>();
+        Speed = 400f;
+        Health = 100;
     }
-    
 
     void FixedUpdate()
     {
-
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector3(moveHorizontal, moveVertical);
-        //transform.Translate(movement * Speed * Time.deltaTime);
-        thisRigidBody.MovePosition(thisRigidBody.position + movement * Speed * Time.fixedDeltaTime);// * Speed * Time.deltaTime);
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        ThisRigidbody.MovePosition(ThisRigidbody.position + movement * Speed * Time.fixedDeltaTime);
+
+        CheckHealth(HealthText, Health);
     }
+
+    void LateUpdate() 
+    {
+        Score += 1;
+        AddScore(ScoreText, Score);
+    }
+
+    void CheckHealth(TextMeshProUGUI healthText, int health)
+    {
+        healthText.text = "Health: " + health.ToString();
+    }
+
+    void AddScore(TextMeshProUGUI scoreText, int score)
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void RemoveHealth(int health)
+    {
+        Health -=1;
+    }
+
+    // called when the cube hits the floor
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if(col.gameObject.name.Contains("Enemy"))
+        {
+            RemoveHealth(1);
+        }
+    }
+
 }
