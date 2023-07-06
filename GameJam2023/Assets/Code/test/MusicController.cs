9064cebe1b9cc94ca97ee6770135343d898da282
Jacob <kubagaczko102@wp.pl>
2023-07-06 17:55:34 +0200

    added music controller

---
+++ GameJam2023/Assets/Code/test/MusicController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicController : MonoBehaviour
+{
+    private AudioSource audioSource;
+    public Slider volumeSlider;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        SetVolume(50);
+        PlayMusic();
+        volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+    }
+
+    public void PlayMusic()
+    {
+        audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        audioSource.Stop();
+    }
+
+    public void SetVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
+
+    void OnVolumeChanged(float volume)
+    {
+        SetVolume(volume);
+    }
+
+}
