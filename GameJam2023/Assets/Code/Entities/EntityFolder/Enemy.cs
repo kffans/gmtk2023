f48c3af870c035cc78234cb9a4679b27356fb4db
kffans <kubakwan@gmail.com>
2023-07-08 13:33:05 +0200

    Meta

--- GameJam2023/Assets/Code/Entities/EntityFolder/Enemy.cs
+++ GameJam2023/Assets/Code/Entities/EntityFolder/Enemy.cs
@@ -1,34 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public abstract class Enemy : Entity
 {
-    private GameObject targetObject;
-	private int health;
+    public GameObject targetObject;
+	public int health;
    
-    void Start()
-    {
-		targetObject = GameObject.Find("Player");
-        //thisRigidBody = this.GetComponent<Rigidbody2D>();
 
-        Event.Move(this.gameObject, 400f, 120, Vector2.left);
-    }
-
-    void FixedUpdate()
-    {
-        if (targetObject != null)
-        {
-            //Follow(targetObject);
-        }
-    }
-
-    void Follow(GameObject myObject)
-    {
-        //Vector2 direction = myObject.transform.position - transform.position;
-        //Vector2 normalizedDirection = direction.normalized;
-		
-        //thisRigidBody.MovePosition(thisRigidBody.position + normalizedDirection * speed * Time.fixedDeltaTime);// * Speed * Time.deltaTime);
-    }
+    //public abstract void Follow();
 }
--- GameJam2023/Assets/Code/Entities/EntityFolder/EnemyFolder/Knight.cs
+++ GameJam2023/Assets/Code/Entities/EntityFolder/EnemyFolder/Knight.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Knight : MonoBehaviour
+public class Knight : Enemy
 {
-    // Start is called before the first frame update
+    public Rigidbody2D thisRigid;
+	
+	// Start is called before the first frame update
     void Start()
     {
-        Event.Move(this.gameObject, 400f, 120, Vector2.left);
+        speed = 400f;
+		targetObject = GameObject.Find("Player");
+        //thisRigid = this.GetComponent<Rigidbody2D>();
+
+		Event.Move(this.gameObject, 400f, 120, Vector2.left);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (targetObject != null)
+        {
+            //Follow(targetObject);
+        }
     }
+	/*public void Follow()
+    {
+        Vector2 direction = targetObject.transform.position - transform.position;
+        Vector2 normalizedDirection = direction.normalized;
+		
+        //thisRigid.MovePosition(thisRigidBody.position + normalizedDirection * speed * Time.fixedDeltaTime);// * Speed * Time.deltaTime);
+    }*/
 }
