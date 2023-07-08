c3e5484fc7bb7d2bb772aea81b3031cd5b7fae2b
Jacob <kubagaczko102@wp.pl>
2023-07-08 18:19:15 +0200

    Furniture And Score Works

--- GameJam2023/Assets/Code/Objects/Furniture.cs
+++ GameJam2023/Assets/Code/Objects/Furniture.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Furniture : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int scoreValue;
+    public TextMeshProUGUI  scoreBoard;
+    public float damageDelay = 2f;
+    private bool canDealDamage = true;
+
+
     void Start()
     {
-        
+        scoreBoard = GameObject.Find("Score").GetComponent<TextMeshProUGUI>();
+        UpdateScoreText(CalculateTotalScore());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateScoreText(CalculateTotalScore());
+        if (scoreValue<= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            StartCoroutine(DealDamageWithDelay());
+        }
+    }
+
+    IEnumerator DealDamageWithDelay()
+    {
+        canDealDamage = false; 
+
+        scoreValue -= 1;
+
+        yield return new WaitForSeconds(damageDelay);
+
+        canDealDamage = true;
+    }
+
+
+
+    int CalculateTotalScore()
+    {
+        Furniture[] furnitureObjects = GameObject.FindObjectsOfType<Furniture>();
+        int totalScore = 0;
+
+        foreach (Furniture furniture in furnitureObjects)
+        {
+            totalScore += furniture.scoreValue;
+        }
+
+        return totalScore;
+    }
+
+    void UpdateScoreText(int score)
+    {
+        scoreBoard.text = "Score: " + score.ToString();
     }
 }
